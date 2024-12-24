6d9f5785d544d5898740293e774eb82f606d323b
Harsh1241 <114918179+Harsh1241@users.noreply.github.com>
2024-12-24 18:06:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,18 @@ const allowedOrigins = [
     'https://traffic-optimizer-j7xlcgkxg-harsh-meenas-projects.vercel.app'
 ];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Allow cookies and credentials
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true, // Allow cookies and credentials
+// }));
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
